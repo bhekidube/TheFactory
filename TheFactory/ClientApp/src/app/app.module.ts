e42d10af1d88568a6598070278be7236e6f57256
bhekinkosidube <bhekinkosidube@gmail.com>
2025-08-19 21:01:45 +0200

    Zimra declaration form

--- TheFactory/ClientApp/src/app/app.module.ts
+++ TheFactory/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BusSearchComponent } from './bus-search/bus-search.component';
+import { ZimraDeclarationComponent } from './zimra-declaration/zimra-declaration.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { BusSearchComponent } from './bus-search/bus-search.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    BusSearchComponent
+    BusSearchComponent,
+    ZimraDeclarationComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ TheFactory/ClientApp/src/app/zimra-declaration/zimra-declaration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zimra-declaration',
+  templateUrl: './zimra-declaration.component.html',
+  styleUrls: ['./zimra-declaration.component.css']
+})
+export class ZimraDeclarationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
