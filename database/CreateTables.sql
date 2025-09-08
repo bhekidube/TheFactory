c69e085d677f5760dff41e1c67d9064166e50076
bhekinkosidube <bhekinkosidube@gmail.com>
2025-09-08 17:41:55 +0200

    big fix

--- database/CreateTables.sql
+++ database/CreateTables.sql
@@ -1,60 +1,60 @@
 CREATE TABLE Country (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    CountryId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Region (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    RegionId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     CountryId INT NOT NULL,
-    FOREIGN KEY (CountryId) REFERENCES Country(Id)
+    FOREIGN KEY (CountryId) REFERENCES Country(CountryId)
 );
 
 CREATE TABLE Town (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    TownId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     RegionId INT NOT NULL,
-    FOREIGN KEY (RegionId) REFERENCES Region(Id)
+    FOREIGN KEY (RegionId) REFERENCES Region(RegionId)
 );
 
 CREATE TABLE LocationType (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    LocationTypeId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Location (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    LocationId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     TownId INT NOT NULL,
     RegionId INT NOT NULL,
     CountryId INT NOT NULL,
     LocationTypeId INT NOT NULL,
-    FOREIGN KEY (TownId) REFERENCES Town(Id),
-    FOREIGN KEY (RegionId) REFERENCES Region(Id),
-    FOREIGN KEY (CountryId) REFERENCES Country(Id),
-    FOREIGN KEY (LocationTypeId) REFERENCES LocationType(Id)
+    FOREIGN KEY (TownId) REFERENCES Town(TownId),
+    FOREIGN KEY (RegionId) REFERENCES Region(RegionId),
+    FOREIGN KEY (CountryId) REFERENCES Country(CountryId),
+    FOREIGN KEY (LocationTypeId) REFERENCES LocationType(LocationTypeId)
 );
 
 CREATE TABLE UserType (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    UserTypeId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE [User] (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
     UserTypeId INT NOT NULL,
     Name NVARCHAR(100) NOT NULL,
     Email NVARCHAR(100),
     CellPhoneNo NVARCHAR(30),
     AlternateCellPhoneNo NVARCHAR(30),
-    FOREIGN KEY (UserTypeId) REFERENCES UserType(Id)
+    FOREIGN KEY (UserTypeId) REFERENCES UserType(UserTypeId)
 );
 
 CREATE TABLE Route (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    RouteId INT IDENTITY(1,1) PRIMARY KEY,
     BusName NVARCHAR(100) NOT NULL,
-    FromTownId INT NOT NULL,
-    ToTownId INT NOT NULL,
+    FromId INT NOT NULL,
+    ToId INT NOT NULL,
     Date DATE NOT NULL,
     DepartureTime NVARCHAR(10) NOT NULL,
     ArrivalTime NVARCHAR(10) NOT NULL,
@@ -63,33 +63,33 @@ CREATE TABLE Route (
     CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
     UpdatedBy INT NULL,
     UpdatedDate DATETIME NULL,
-    FOREIGN KEY (FromTownId) REFERENCES Town(Id),
-    FOREIGN KEY (ToTownId) REFERENCES Town(Id),
-    FOREIGN KEY (CreatedBy) REFERENCES [User](Id),
-    FOREIGN KEY (UpdatedBy) REFERENCES [User](Id)
+    FOREIGN KEY (FromId) REFERENCES Town(TownId),
+    FOREIGN KEY (ToId) REFERENCES Town(TownId),
+    FOREIGN KEY (CreatedBy) REFERENCES [User](UserId),
+    FOREIGN KEY (UpdatedBy) REFERENCES [User](UserId)
 );
 
 CREATE TABLE OperatorType (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OperatorTypeId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Operator (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OperatorId INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
     OperatorTypeId INT NOT NULL,
-    FOREIGN KEY (OperatorTypeId) REFERENCES OperatorType(Id)
+    FOREIGN KEY (OperatorTypeId) REFERENCES OperatorType(OperatorTypeId)
 );
 
 
 
 CREATE TABLE OperatorContact (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OperatorContactId INT IDENTITY(1,1) PRIMARY KEY,
     OperatorId INT NOT NULL,
     Email NVARCHAR(100),
     CellPhoneNo NVARCHAR(30),
     Address NVARCHAR(200),
     OperatorContactPersonId INT NOT NULL,
-    FOREIGN KEY (OperatorId) REFERENCES Operator(Id),
-    FOREIGN KEY (OperatorContactPersonId) REFERENCES [User](Id)
+    FOREIGN KEY (OperatorId) REFERENCES Operator(OperatorId),
+    FOREIGN KEY (OperatorContactPersonId) REFERENCES [User](UserId)
 );
