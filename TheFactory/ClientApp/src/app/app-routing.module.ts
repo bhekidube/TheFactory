c5aeda936a7f4bbcbff119f48f8f659c95a5b047
bhekinkosidube <bhekinkosidube@gmail.com>
2026-02-18 23:47:24 +0200

    ZIMRA - calculator

--- TheFactory/ClientApp/src/app/app-routing.module.ts
+++ TheFactory/ClientApp/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component'; 
 import { AdminScreenComponent } from './admin-screen/admin-screen.component';
 import { OperatorAdminComponent } from './operator-admin/operator-admin.component';
+import { ZimraComponent } from './zimra/zimra.component';
+import { FaresComponent } from './fares/fares.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent, pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
   { path: 'admin-screen', component: AdminScreenComponent },
-  { path: 'operator-admin/:operator', component: OperatorAdminComponent }
+  { path: 'operator-admin/:operator', component: OperatorAdminComponent },
+  { path: 'zimra-guide-2026', component: ZimraComponent },
+  { path: 'bus-fares-jhb-bulawayo', component: FaresComponent },
   // ...other routes
 ];
 
