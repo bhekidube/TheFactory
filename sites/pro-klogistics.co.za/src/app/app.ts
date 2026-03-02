7ebac5e744d804a35eb71dbbf50cc1e0b840cbcd
bhekinkosidube <bhekinkosidube@gmail.com>
2026-03-02 22:29:22 +0200

    Eanble Lucide icons

--- sites/pro-klogistics.co.za/src/app/app.ts
+++ sites/pro-klogistics.co.za/src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from './banner.component';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BannerComponent],
+  imports: [
+    RouterOutlet,
+    BannerComponent,
+    LucideAngularModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
