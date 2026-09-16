c0ef92a8872d26e7319e58d8c080ce1db37437a0
bhekinkosidube <bhekinkosidube@gmail.com>
2026-09-16 16:59:29 +0200

    Hambaonline - make taglines conditional

--- TheFactory/ClientApp/src/app/app.component.ts
+++ TheFactory/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ export class AppComponent {
   title = 'app';
   showOperatorRoute = false;
   currentYear = new Date().getFullYear();
+
+  constructor(private readonly router: Router) {}
+
+  get showTransportTagline(): boolean {
+    const currentPath = this.router.url.split('?')[0].toLowerCase();
+    return !currentPath.startsWith('/learning') && !currentPath.startsWith('/school/admin');
+  }
+
   // Call this method when you want to show the operator route component
   toggleOperatorRoute() {
     this.showOperatorRoute = !this.showOperatorRoute;
